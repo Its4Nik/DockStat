a24eef23b564b20d2b4de81dad9984d850149bba
Its4Nik <info@itsnik.de>
2025-08-17 04:51:11 +0200

    feat(ci/cd): logging for sync.sh

--- apps/docs/sync.sh
+++ apps/docs/sync.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+echo "Starting sync"
+echo "Previous page file:"
+cat pages.json
+
 # Init files
 bunx @dockstat/outline-sync@latest \
     --init \
     --collection-id="$OUTLINE_COLLECTION_ID" \
     --api-key="$OUTLINE_API_KEY" \
     --base-url="https://outline.itsnik.de"
+
+echo "New page file:"
+cat pages.json
 
 # Sync files
 bunx @dockstat/outline-sync@latest \
