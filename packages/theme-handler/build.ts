495a16f86db3ebe2fe7e6e3336863073d68d3c81
Its4Nik <info@itsnik.de>
2025-08-24 19:40:25 +0200

    🛠️ Build: Configure build process with Bun
    Configures the build process for the theme-handler package using Bun. Includes dts plugin for generating declaration files, minification, target setting, and sourcemap generation. Also adds a prepublishOnly script to ensure the package is built before publishing.

---
+++ packages/theme-handler/build.ts
@@ -0,0 +1,22 @@
+import dts from 'bun-plugin-dts'
+
+function now() {
+  return new Date()
+}
+
+const NOW = now()
+
+console.info(`Start at ${NOW}`)
+
+await Bun.build({
+  entrypoints: ['./index.ts'],
+  outdir: './dist',
+  plugins: [dts()],
+  minify: true,
+  target: 'node',
+  sourcemap: 'inline',
+})
+
+console.info(
+  `Done at ${now()} - took ${new Date(now().getTime() - NOW.getTime()).getSeconds()}`
+)
