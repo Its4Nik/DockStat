27562f0f3b1946021ff02781622322c570ecb22c
ItsNik <niklaskrause@duck.com>
2025-10-31 00:34:22 +0100

    feat(deps): Update dependencies and add bunfig.toml files
    This commit updates dependencies in `package.json` and `bun.lock` to their latest versions. It also adds `bunfig.toml` files to various packages and apps to configure bun with `linker = "isolated"`
    
    The `install-deps.sh` script was removed.

--- apps/dockstat/app/.server/api/plugins/index.ts
+++ apps/dockstat/app/.server/api/plugins/index.ts
@@ -13,22 +13,43 @@ export const PluginHandler = new PluginHandlerFactory(
 
 const PluginTable: QueryBuilder<DBPluginShemaT> = PluginHandler.getTable();
 
-const PluginElysiaInstance = new Elysia({ prefix: "/plugins", detail: { tags: ["Plugins"] } })
+const PluginElysiaInstance = new Elysia({
+  prefix: "/plugins",
+  detail: { tags: ["Plugins"] },
+})
   .get("/all", () => PluginTable.select(["*"]).all())
-  .get('/status', () => PluginHandler.getStatus())
-  .post("/install", ({ body }) => PluginHandler.savePlugin(body), { body: DBPluginShema })
-  .post("/install/url", ({ body }) => PluginHandler.installFromManifestLink(body), { body: t.String() })
-  .post("/activate", async ({ body }) => await PluginHandler.loadPlugins(body),
+  .get("/status", () => PluginHandler.getStatus())
+  .post("/install", ({ body }) => PluginHandler.savePlugin(body), {
+    body: DBPluginShema,
+  })
+  .post(
+    "/install/url",
+    ({ body }) => PluginHandler.installFromManifestLink(body),
+    { body: t.String() }
+  )
+  .post(
+    "/activate",
+    async ({ body }) => await PluginHandler.loadPlugins(body),
     {
       body: t.Array(t.Number()),
-      response: t.Object(
-        {
-          successes: t.Array(t.Number()),
-          errors: t.Array(t.Number())
-        })
-    })
-  .post("/delete", ({ body }) => PluginHandler.deletePlugin(body), { body: t.Number() })
-  .post("/:id/routes/*", async ({ request, params }) => PluginHandler.handleRoute(request, params), { detail: { description: "This route proxies all Plugin-API requests to the specified Plugin's Elysia Instance" } });
-
+      response: t.Object({
+        successes: t.Array(t.Number()),
+        errors: t.Array(t.Number()),
+      }),
+    }
+  )
+  .post("/delete", ({ body }) => PluginHandler.deletePlugin(body), {
+    body: t.Number(),
+  })
+  .post(
+    "/:id/routes/*",
+    async ({ request, params }) => PluginHandler.handleRoute(request, params),
+    {
+      detail: {
+        description:
+          "This route proxies all Plugin-API requests to the specified Plugin's Elysia Instance",
+      },
+    }
+  );
 
 export default PluginElysiaInstance;
