0b8844ee16e8a1977ff7d08cea09b3b159d3a2fc
ItsNik <info@itsnik.de>
2026-01-08 18:56:31 +0100

    Update packages/docker-client/src/manager/core.ts
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Signed-off-by: ItsNik <info@itsnik.de>

--- packages/docker-client/src/manager/core.ts
+++ packages/docker-client/src/manager/core.ts
@@ -597,24 +597,18 @@ export class DockerClientManagerCore {
 
   public internalListeners(wrapper: WorkerWrapper, msg: EventMessage<keyof EVENTS>) {
     switch (msg.type) {
-      case "host:init": {
-        this.logger.debug(`Received ${msg.type} - adding host to Set: wrapper.hostIds`)
-        const hostId = (msg.ctx as Parameters<EVENTS["host:added"]>[0]).hostId
-        !wrapper.hostIds.has(hostId) && wrapper.hostIds.add(hostId)
-        break
-      }
-
+      case "host:init":
       case "host:added": {
+        const hostId = (msg.ctx as Parameters<EVENTS[typeof msg.type]>[0]).hostId
         this.logger.debug(`Received ${msg.type} - adding host to Set: wrapper.hostIds`)
-        const hostId = (msg.ctx as Parameters<EVENTS["host:added"]>[0]).hostId
-        !wrapper.hostIds.has(hostId) && wrapper.hostIds.add(hostId)
+        wrapper.hostIds.add(hostId)
         break
       }
 
       case "host:removed": {
-        this.logger.debug(`Received ${msg.type} - removing host from Set: wrapper.hostIds`)
         const hostId = (msg.ctx as Parameters<EVENTS["host:removed"]>[0]).hostId
-        wrapper.hostIds.has(hostId) && wrapper.hostIds.delete(hostId)
+        this.logger.debug("Received host:removed - removing host from Set: wrapper.hostIds")
+        wrapper.hostIds.delete(hostId)
         break
       }
     }
