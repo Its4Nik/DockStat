235a2822c39569d9a40b34815a27f955984a7c95
actions-user <its4nik@users.noreply.github.com>
2026-02-08 03:17:51 +0000

    chore(ci): Lint [skip ci]

--- packages/utils/src/react/eden/index.ts
+++ packages/utils/src/react/eden/index.ts
@@ -1,5 +1,5 @@
+export * from "./types"
 export * from "./useBaseEdenMutation"
 export * from "./useEdenMutation"
 export * from "./useEdenQuery"
 export * from "./useEdenRouteMutation"
-export * from "./types"
